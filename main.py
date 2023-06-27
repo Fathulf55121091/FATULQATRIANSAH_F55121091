d28aa341e3c799e0f13b993ca5494865ebbc9980
Fathulf55121091 <116184188+Fathulf55121091@users.noreply.github.com>
2023-06-27 23:06:38 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+import sys
 
-def insertion_sort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+def dijkstra(graph, start):
+    shortest_distance = {}
+    shortest_path = {}
+    for node in graph:
+        shortest_distance[node] = float('inf')
+    shortest_distance[start] = 0
+    shortest_path[start] = []
 
-def print_array(arr):
-    for element in arr:
-        print(element, end=" ")
-    print()
+    while graph:
+        min_node = None
+        for node in graph:
+            if min_node is None:
+                min_node = node
+            elif shortest_distance[node] < shortest_distance[min_node]:
+                min_node = node
 
-def main():
-    print("Selamat datang di program pengurutan!")
-    print("Pilih algoritma pengurutan yang ingin digunakan:")
-    print("1. Bubble Sort")
-    print("2. Insertion Sort")
-    choice = input("Masukkan pilihan (1/2): ")
+        current_distance = shortest_distance[min_node]
+        for neighbor, weight in graph[min_node].items():
+            distance = current_distance + weight
+            if distance < shortest_distance[neighbor]:
+                shortest_distance[neighbor] = distance
+                shortest_path[neighbor] = shortest_path[min_node] + [min_node]
 
-    if choice == '1':
-        # Bubble Sort
-        arr = [64, 34, 25, 12, 22, 11, 90]
-        print("Array sebelum pengurutan:")
-        print_array(arr)
-        bubble_sort(arr)
-        print("Array setelah pengurutan menggunakan Bubble Sort:")
-        print_array(arr)
-    elif choice == '2':
-        # Insertion Sort
-        arr = [64, 34, 25, 12, 22, 11, 90]
-        print("Array sebelum pengurutan:")
-        print_array(arr)
-        insertion_sort(arr)
-        print("Array setelah pengurutan menggunakan Insertion Sort:")
-        print_array(arr)
-    else:
-        print("Pilihan tidak valid. Silakan coba lagi.")
+        del graph[min_node]
 
-if _name_ == "_main_":
-    main()
+    return shortest_distance, shortest_path
+
+# Kasus Terburuk (Worst Case)
+graph = {
+    'A': {'B': 1, 'C': 1, 'D': 1, 'E': 1},
+    'B': {'A': 1, 'C': 1, 'D': 1, 'E': 1},
+    'C': {'A': 1, 'B': 1, 'D': 1, 'E': 1},
+    'D': {'A': 1, 'B': 1, 'C': 1, 'E': 1},
+    'E': {'A': 1, 'B': 1, 'C': 1, 'D': 1}
+}
+
+start_node = 'A'
+distances, paths = dijkstra(graph, start_node)
+
+print("Shortest distances:")
+for node, distance in distances.items():
+    print(f"To node {node}: {distance}")
+
+print("Shortest paths:")
+for node, path in paths.items():
+    print(f"To node {node}: {' -> '.join(path + [node])}")
